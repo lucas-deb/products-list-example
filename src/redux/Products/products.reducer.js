014f7745c692d9bb6ba2fa3addcffdd3dde34b91
Lucas de Barros <lucas.deb.d3v@gmail.com>
2021-10-04 12:32:54 -0300

    Removed unused ids

--- src/redux/Products/products.reducer.js
+++ src/redux/Products/products.reducer.js
@@ -3,22 +3,18 @@ import { GET_PRODUCT } from "./products.types";
 const INITIAL_STATE = {
   list: [
     {
-      id: 0,
       name: "First Product",
       description: "This is a first description",
     },
     {
-      id: 1,
       name: "Second Product",
       description: "This is a second description",
     },
     {
-      id: 2,
       name: "Third Product",
       description: "This is a third description",
     },
     {
-      id: 3,
       name: "Fourth Product",
       description: "This is a fourth description",
     },
