befe8cb388097bc4329740c467405ca517dd9e28
Lucas de Barros <lucas.deb.d3v@gmail.com>
2021-10-04 11:54:07 -0300

    Products details:

--- src/pages/Home/Details/index.jsx
+++ src/pages/Home/Details/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Button, Card, Container } from "react-bootstrap";
+import { useParams, Link } from "react-router-dom";
 
 export default () => {
   const { id } = useParams();
-  const foundProduct = useSelector((state) =>
-    state.products.list.find((product) => product.id === Number(id))
+  const product = useSelector((state) =>
+    state.products.list.find((item) => item.id === Number(id))
   );
 
-  return <div>{foundProduct.name}</div>;
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Link to="/">
+            <Button variant="outline-primary">Go Back</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
